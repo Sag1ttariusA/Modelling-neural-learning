a53031aa7a63ab19529a0f3a541bc48e944982cc
Sag1ttariusA <lukas@jeanm.de>
2025-10-04 09:46:08 +0200

    integrated low level Integrate and fire

--- Neuron_modelling.md
+++ Neuron_modelling.md
@@ -4,7 +4,10 @@
 	- [x] have it fire at some probability otherwise have it rest
 	- [x] create a function that increases the likelihood that one neuron will fire, if another fires
  	- [x] create a neuron class
-  	- [ ] upgrade the firing mechanism
-  		- [ ] neuron a that randomly fires -> primary sensory neuron
-  	 	- [ ] neuron b's membrane potential is increased if neuron a fires and decreased or unaltered if neuron a restst
-  	  	- [ ] if neuron b's membrane potential is reached, then it fires 	
+  	- [x] upgrade the firing mechanism
+  		- [x] neuron a that randomly fires -> primary sensory neuron
+  	 	- [x] neuron b's membrane potential is increased if neuron a fires and decreased or unaltered if neuron a restst
+  	  	- [x] if neuron b's membrane potential is reached, then it fires 	
+
+- [ ] **Storing the state of the system** 
+-> the systems state should incorporate all the details which are needed to run the next step of the simulation -> state determines next run through
--- neuron.py
+++ neuron.py
@@ -15,31 +15,36 @@ class Primary_Neuron:
     
 
 class Secondary_Neuron:
-    def __init__(self, name, resting_potential, threshold, signal_strength):       
+    def __init__(self, name, resting_potential, threshold, signal_strength, post_fire_potential):       
         self.name = name
         self.membrane_potential = resting_potential
         self.threshold = threshold
-        self.signal_strength = signal_strength
+        self.signal_strength = signal_strength  # size of the step increase if a an action potential is received
+        self.post_fire_potential = post_fire_potential
        
     def simulate(self, input):
         if input == "Fires":
             self.membrane_potential += self.signal_strength  
+        # if the neuron reaches threshold it fires and its membrane potential is set back down
         if self.membrane_potential >= self.threshold:
-            return f"{self.name}: {self.membrane_potential} --> Fires!"
+            temp = self.membrane_potential
+            self.membrane_potential = self.post_fire_potential
+            return f"{self.name}: {temp} --> Fires!"
         else: 
             return f"{self.name}: {self.membrane_potential} --> Rests"
 
+neuron_a = Primary_Neuron("Neuron_A", 0.4, 1.2)
+neuron_b = Secondary_Neuron("Neuron_B", -60, 10, 20, -100)
 
 def main():
-    neuron_a = Primary_Neuron("Neuron_A", 1, 1.2)
-    neuron_b = Secondary_Neuron("Neuron_B", -60, 10, 20)
     neuron_a_outpout = neuron_a.simulate()
     neuron_b_input = neuron_a.result
     neuron_b_output = neuron_b.simulate(neuron_b_input)
     output = [neuron_a_outpout, neuron_b_output]
     print(output)
 
-main()
+for _ in range(10): 
+    main()
     
 
 
